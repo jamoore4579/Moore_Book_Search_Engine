f9098ccb4eca227d62da367c019a3977ba481d08
Irish Moore <jasonamoore@frontier.com>
2022-04-29 16:04:50 -0400

    code index and resolver files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,59 @@
+const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../util/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user_id })
+                .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+    
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne( { email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user_id },
+                    { $addToSet: {savedBooks: book} },
+                    { new: true }
+                )
+                return updateUser;
+            }
+            throw new AuthenticationError('You need to be logged in')
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                )
+                return updateUser;
+            }
+        }
+    }
+},
+
+module.exports = resolvers;
