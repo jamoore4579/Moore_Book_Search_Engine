f4ab07520942a9c2426b9b890c4c6242ce2269e2
Irish Moore <jasonamoore@frontier.com>
2022-04-30 23:45:24 -0400

    corrected some code on client side and now is client is working

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -4,7 +4,6 @@ import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations'
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
