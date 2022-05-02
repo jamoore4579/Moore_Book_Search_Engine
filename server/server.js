06cc0dfbd9883318f18cbbed5a89741e7889d3b4
Irish Moore <jasonamoore@frontier.com>
2022-05-01 20:34:49 -0400

    update server file

--- server/server.js
+++ server/server.js
@@ -6,19 +6,23 @@ const { typeDefs, resolvers } = require('./schemas')
 const {authMiddleware} = require('./utils/auth');
 
 const db = require('./config/connection');
-//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // apollo server
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQl at http://loclahost:${PORT}${server.graphqlPath}`);
+}
 
-server.applyMiddleware({ app });
+startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,11 +36,10 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`)
-    console.log(`Use GraphQl at http://loclahost:${PORT}${server.graphqlPath}`);
   });
 });
